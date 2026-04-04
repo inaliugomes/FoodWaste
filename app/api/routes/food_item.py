036d3e7c8ed7d811a481e0a93b9a57b0222216f0
inaliugomes <inaliugomes@gmail.com>
2026-04-04 11:08:24 +0200

    Delete function implement

--- app/api/routes/food_item.py
+++ app/api/routes/food_item.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import Session
 from app.database.connection import get_db
 from app.database.models import FoodItem
 from app.schemas.food_item import FoodItemCreate, FoodItemResponse
-from app.crud.food_item import create_food_item,get_all_food_items,get_food_item_by_id
+from app.crud.food_item import create_food_item,get_all_food_items,get_food_item_by_id,delete_food_item_by_id
 
 router = APIRouter(
     prefix="/food_item",
@@ -23,3 +23,7 @@ def get_all(db:Session=Depends(get_db)):
 @router.get("/{food_item_id}",response_model=FoodItemResponse)
 def get_food(food_item_id:int,db:Session=Depends(get_db)):
     return get_food_item_by_id(db,food_item_id)
+
+@router.delete("/{food_item_id}",response_model=FoodItemResponse)
+def delete(food_item_id:int,db:Session=Depends(get_db)):
+    return delete_food_item_by_id(db,food_item_id)
--- app/crud/food_item.py
+++ app/crud/food_item.py
@@ -1,8 +1,9 @@
 from fastapi import HTTPException
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, session
 from app.database.models import  FoodItem
 from app.schemas.food_item import FoodItemCreate
 
+#Create Element
 def create_food_item(db:Session,item:FoodItemCreate):
     db_item = FoodItem(**item.model_dump())
     db.add(db_item)
@@ -10,12 +11,24 @@ def create_food_item(db:Session,item:FoodItemCreate):
     db.refresh(db_item)
     return db_item
 
+#Search for all element
 def get_all_food_items(db:Session):
     return db.query(FoodItem).all()
 
+#Get element By ID
 def get_food_item_by_id(db:Session,food_item_id:int):
     food_item = db.query(FoodItem).filter(FoodItem.id == food_item_id).first()
     if not food_item:
         raise HTTPException(status_code=404, detail="FoodItem not found")
 
     return food_item
+
+def delete_food_item_by_id(db:Session,food_item_id:int):
+    food_item = db.query(FoodItem).filter(FoodItem.id == food_item_id).first()
+    if not food_item:
+        raise HTTPException(status_code=404, detail="FoodItem not found")
+    db.delete(food_item)
+    db.commit()
+    return food_item
+
+
