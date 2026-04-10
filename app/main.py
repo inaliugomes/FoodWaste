1c5d832497c4c32edd31e79d938fc0e7a4e77f3a
inaliugomes <inaliugomes@gmail.com>
2026-04-10 18:29:39 +0200

    POST user created

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
-from app.api.routes import food_item
+from app.api.routes import food_item,user
 from app.database.base import Base
 from app.database.connection import engine
 
 app = FastAPI()
 app.include_router(food_item.router)
+app.include_router(user.router)
 #Permite crear/validar si ya existe la base de datos
 Base.metadata.create_all(bind=engine)
