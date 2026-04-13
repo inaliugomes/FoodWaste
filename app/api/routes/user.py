636027b8541116c38acfb76d707c4d3c4164ef06
inaliugomes <inaliugomes@gmail.com>
2026-04-13 23:58:51 +0200

    get user by id

--- app/api/routes/user.py
+++ app/api/routes/user.py
@@ -1,6 +1,6 @@
 from fastapi import Depends,APIRouter,Query
 from sqlalchemy.orm import Session
-from app.crud.user import create_user
+from app.crud.user import create_user,get_all_users,get_user_by_id
 from app.database.connection import get_db
 from app.schemas.user import UserItemResponse, UserCreate
 
@@ -12,3 +12,11 @@ router = APIRouter(
 @router.post("/", response_model=UserItemResponse)
 def create(user:UserCreate, db:Session = Depends(get_db)):
     return create_user(user,db)
+
+@router.get("/", response_model=list[UserItemResponse])
+def get_all_user(db:Session = Depends(get_db)):
+    return get_all_users(db)
+
+@router.get("/{id}", response_model=UserItemResponse)
+def get_user(user_id:int, db:Session = Depends(get_db)):
+    return get_user_by_id(user_id,db)
--- app/crud/user.py
+++ app/crud/user.py
@@ -9,3 +9,19 @@ def create_user(user:UserCreate,db:Session):
     db.commit()
     db.refresh(db_user)
     return db_user
+
+def get_all_users(db:Session):
+
+    users =  db.query(User).all()
+    total = len(users)
+    if users:
+        return {"users":users, "total":total}
+    else:
+        return {"users":[], "total":total}
+
+
+def get_user_by_id(user_id:int, db:Session):
+    user = db.query(User).filter(User.id == user_id).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return {"user":user}
