2d0d3c681045fb7add068ecfb7011278ba863060
inaliugomes <inaliugomes@gmail.com>
2026-05-07 23:42:41 +0200

    sadknfadisd

--- app/api/routes/food_item.py
+++ app/api/routes/food_item.py
@@ -1,6 +1,8 @@
 from fastapi import Depends , APIRouter,Query
 from sqlalchemy.orm import Session
 from typing import Annotated
+from app.crud.auth import get_current_user
+from app.database.models import User
 from app.core.enums import FoodNameEnum, CategoryEnum
 from app.database.connection import get_db
 from app.schemas.food_item import FoodItemCreate, FoodItemResponse, FoodItemUpdate, FoodItemListResponse
@@ -22,7 +24,7 @@ def get_all(skip:Annotated[int,Query(ge=0)]=0,
             food_name:Annotated[FoodNameEnum | None,Query()]=None,
             food_category:Annotated[CategoryEnum |None,Query()]=None,
             food_quantity:Annotated[int |None ,Query()]=None,
-            db:Session=Depends(get_db)):
+            db:Session=Depends(get_db),current_user: User = Depends(get_current_user)):
     return get_all_food_items(db,skip=skip,limit=limit,
                               food_name=food_name,
                               food_category=food_category,
